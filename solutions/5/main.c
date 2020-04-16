d49857de3e52be2a96f5c5ecb9654713c90a4ed4
viitana4 <atte.viitanen@aalto.fi>
2020-04-16 20:57:51 +0300

    Improve problem 5 solution

--- solutions/5/main.c
+++ solutions/5/main.c
@@ -15,24 +15,21 @@ int main(int argc, char** argv)
 
     printf("Calculating The smallest number that can be divided by each of the numbers from 1 to %d\n", n);
 
-    unsigned long long max = 1u;
-    for (int i = 2; i <= n; i++)
-    {
-        max *= i;
-    }
-
-    unsigned long long result = max;
-
-    for (unsigned long long l = max-1u; l > 0u; l--)
+    unsigned long long current = n;
+    for (;;)
     {
         bool divisible = true;
         for (unsigned u = 1u; u <= n; u++)
         {
-            if (l % u != 0) divisible = false;
+            if (current % u != 0) divisible = false;
         }
-        if (divisible) result = l;
+        if (divisible)
+        {
+            break;
+        }
+        current++;
     }
 
-    printf("Result: %llu\n", result);
+    printf("Result: %llu\n", current);
     return 0;
 }
